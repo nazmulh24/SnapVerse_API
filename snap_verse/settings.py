44fe61f8bef4c87be3c184295c5a90d2da214563
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-23 21:17:17 +0600

    Configure Django Debug Toolbar

--- snap_verse/settings.py
+++ snap_verse/settings.py
@@ -29,13 +29,21 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #
+    "debug_toolbar",
+    #
     "api",
     "users",
     "posts",
     "relationships",
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    #
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
