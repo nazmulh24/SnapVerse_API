ad7bac222df6da0ba3d6d0f8be8775f1ad272def
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-24 06:34:24 +0600

    users api done..

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
+
+from users.views import UserViewSet
 
 
 router = routers.DefaultRouter()  # ----> Api Root a error day na...link day...
+
+router.register("users", UserViewSet, basename="users")
 
 
 urlpatterns = [
     path("", include(router.urls)),
     # path
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
 ]
