1543f8240c2a342fdd98161bf263f610dd3c9ba7
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-09-11 21:48:49 +0600

    trying to fix image..

--- posts/serializers.py
+++ posts/serializers.py
@@ -75,6 +75,9 @@ class PostUpdateSerializer(serializers.ModelSerializer):
         fields = ["caption", "location", "privacy", "image"]
 
     def update(self, instance, validated_data):
+        if "image" in validated_data and not validated_data["image"]:
+            validated_data.pop("image")
+
         validated_data["is_edited"] = True
         return super().update(instance, validated_data)
 
